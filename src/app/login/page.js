a422b453ad3f916c12a281369641e15a6b0214c6
Miguel <Miguel.giordano@solera.com>
2024-10-25 11:55:38 +0200

    redirect correction

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -7,7 +7,7 @@ export default function Login() {
         <div className="container mx-auto p-4 text-center">
           <h1 className="text-3xl font-bold mb-4">Iniciar Sesión</h1>
           <button
-            onClick={() => signIn(undefined, { callbackUrl: "/dashboard" })}
+            onClick={() => signIn(undefined, { callbackUrl: "/" })}
             className="px-4 py-2 bg-blue-500 text-white rounded"
           >
             Sign in with GitHub
